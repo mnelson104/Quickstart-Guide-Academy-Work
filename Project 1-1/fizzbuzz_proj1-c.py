676b9204844a0a0179ae520d204f4d21ec3809fe
mnelson104 <mnelson104@gmail.com>
2025-04-18 19:53:25 -0400

    updating to latest

---
+++ Project 1-1/fizzbuzz_proj1-c.py
@@ -0,0 +1,18 @@
+def fizzbuzz(x_range):
+    fizzbuzz_array = []
+    #go through all values from 1 through max value
+    for x in range(1,x_range+1):
+        print_value = ""
+        if ((x % 3) ==0):
+            print_value += "fizz"
+        if ((x % 5) == 0):
+            print_value += "buzz"
+        if((x % 5) != 0) and ((x % 3) != 0):
+            print_value = x
+        fizzbuzz_array.append(print_value)
+    return fizzbuzz_array
+
+fizzbuzzcount = int(input("please enter the value you want to count up to:"))
+myfizzbuzz = fizzbuzz(fizzbuzzcount)
+for fizz in myfizzbuzz:
+    print(fizz)
